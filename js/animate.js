8e69ced34b628d3ad6f3d3eb3275f423d56aceb4
PetraAleksandar <aleksandar.debeljak.91@gmail.com>
2017-09-08 15:58:09 +0200

    Changed header css, and added animate.js

---
+++ js/animate.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {
+        $('.headerContainer').css('height','80px');
+        $('.logo').css('font-size','35px');
+        $('.nav_item').css('padding-top','35px');
+        $('.social').css('padding-top','35px');
+      }
+      else{
+        $('.headerContainer').css('height','110px');
+        $('.logo').css('font-size','45px');
+        $('.nav_item').css('padding-top','45px');
+        $('.social').css('padding-top','45px');
+      }
+});
